50bff934c616ded41ced45d88759b13ba2942a50
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2026-03-08 20:32:16 -0300

    Trabalhando com Variáveis e Constantes

---
+++ exercicio/ex01.swift
@@ -0,0 +1,10 @@
+// Crie uma constante nome e uma variável idade. Atribua valores a elas e imprima os
+// valores na tela. Depois, altere o valor da variável idade e imprima novamente.
+// Objetivo: Trabalhar com variáveis e constantes.
+
+let nome = "Maria"
+var idade = 23
+print("Nome: \(nome), Idade: \(idade)")
+idade = 24
+print("Nome: \(nome), Idade: \(idade)")
+
