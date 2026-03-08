51faad3e68da0c222c8d5327e98480560ed6929b
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2026-03-08 20:45:02 -0300

    Conceito: Funções

---
+++ guia/aula04.swift
@@ -0,0 +1,9 @@
+// As funções em Swift são blocos de código que podem ser chamados para executar
+// uma tarefa específica. Elas podem receber parâmetros e retornar valores.
+// • func: Palavra-chave usada para declarar uma função.
+// • Parâmetros: Valores passados para a função.
+// • Retorno: A função pode retornar um valor com a palavra-chave return
+
+func adicionaUm(_ x: Int) -> Int {
+    return x + 1
+}
