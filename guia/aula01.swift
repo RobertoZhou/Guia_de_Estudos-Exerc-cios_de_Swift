167fe072637917d28a7f0dd36cfda54e840782e2
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2026-03-08 20:31:22 -0300

    Conceito: Variáveis e Constantes

---
+++ guia/aula01.swift
@@ -0,0 +1,15 @@
+// Swift permite declarar variáveis e constantes. As variáveis podem ser alteradas ao
+// longo do tempo, enquanto as constantes são valores imutáveis depois de atribuídos.
+// • var: Usado para declarar uma variável, cujo valor pode ser alterado.
+// • let: Usado para declarar uma constante, cujo valor não pode ser alterado após
+// ser atribuído.
+
+// Declarando uma constante e uam variável
+let pessoa = "Alice"  // Constante
+var ano = 2024      // Variável
+
+print("Pessoa: \(pessoa), Ano: \(ano)")
+
+// Alterando o valor da variável
+ano = 2025
+print("Pessoa: \(pessoa), Ano: \(ano)")
