d1357d4dcf238e6f16430cb46cbf8ee64823e39d
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2026-03-08 20:49:28 -0300

    Criando uma Função para Somar Números

---
+++ exercicio/ex04.swift
@@ -0,0 +1,9 @@
+// Crie uma função chamada soma que recebe dois números inteiros como parâmetros e
+// retorna a soma deles.
+// Objetivo: Trabalhar com funções e parâmetros
+
+func soma(_ num1: Int, _ num2: Int) -> Int{
+    return num1 + num2
+}
+
+print(soma(2, 3))
