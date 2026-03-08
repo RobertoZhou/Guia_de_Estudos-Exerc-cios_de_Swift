652b1fc6571b8ebeedcdfbcffd9d929e2ab83745
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2026-03-08 20:34:53 -0300

    Conceito: Condicionais (if/else)

---
+++ guia/aula02.swift
@@ -0,0 +1,15 @@
+// As condicionais permitem executar um bloco de código com base em uma condição. O
+// if/else é usado para tomar decisões, verificando se a condição é verdadeira ou falsa.
+// • if: Avalia uma condição.
+// • else: Define o que acontecerá quando a condição não for verdadeira.
+// • else if: Usado quando há mais de uma condição a ser verificada.
+
+let numero = 5
+
+if numero == 0 {
+    print("O número e 0")
+} else if numero == 1 {
+    print("O número e 1")
+} else {
+    print("O número não é 0 e 1")
+}
