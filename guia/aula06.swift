47c4a0efcef94e5de5f5a4bee2672d2ca717c1c0
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2026-03-08 23:07:16 -0300

    Conceito: Dicionários

---
+++ guia/aula06.swift
@@ -0,0 +1,8 @@
+// Um dicionário em Swift é uma coleção não ordenada de pares chave-valor. Ele é útil
+// para armazenar dados que podem ser recuperados rapidamente por uma chave única.
+// • [:]: Usado para definir um dicionário.
+// • Chave e valor: Cada elemento do dicionário é um par chave-valor.
+
+var paises = ["BR": "Brasil", "EUA": "Estados Unidos", "ARG": "Argentina", "FRA": "França"]
+
+print(paises["BR"]!) // Brasil
