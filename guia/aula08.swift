f60a94a51a822e4daa51b809b33dac72edee01ed
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2026-03-08 23:52:41 -0300

    Conceito: Switch Case

---
+++ guia/aula08.swift
@@ -0,0 +1,16 @@
+// O switch case permite testar várias condições de forma eficiente. Ele pode ser usado
+// para verificar intervalos, valores específicos, ou condições complexas.
+// • case: Define um valor ou condição a ser verificada.
+// • default: Caso nenhum case seja verdadeiro.
+
+
+let nome = "Mark"
+
+switch nome {
+    case "Mark":
+        print("Olá, Mark!")
+    case "João":
+        print("Olá, João!")
+    default:
+        print("Olá, Estranho!")
+}
