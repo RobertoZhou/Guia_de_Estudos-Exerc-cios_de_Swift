6984a287c88ae7493fbf39b67bfee803b557b695
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2026-03-08 23:10:39 -0300

    Criando e Acessando um Dicionário

---
+++ exercicio/ex06.swift
@@ -0,0 +1,8 @@
+// Crie um dicionário que armazene nomes de países como chaves e suas capitais como
+// valores. Em seguida, imprima a capital de um país específico.
+// Objetivo: Trabalhar com dicionários.
+
+var paises = ["BR": "Brasil", "EUA": "Estados Unidos", "ARG":
+"Argentina", "FRA": "França"]
+
+print(paises["FRA"]!)
