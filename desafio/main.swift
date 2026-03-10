ec35163f6f0a9585f74584e4b010aca2bec82d9a
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2026-03-09 21:09:37 -0300

    Criando sistema de empréstimo

--- desafio/main.swift
+++ desafio/main.swift
@@ -56,8 +56,49 @@ while true {
             
             for livro in livros {
                 print("Título: \(livro.titulo) - Autor: \(livro.autor) - Ano de Públicação: \(livro.anoPublicacao)")
+                if livro.disponibilidade {
+                    print("Disponível: Sim")
+                } else {
+                    print("Diponível: Não")
+                }
+            }
+        case "3":
+            print("===== VERIFICAR STATUS DO LIVRO =====")
+        
+            print("Digite o nome do livro:")
+            let nomeLivro = readLine() ?? ""
+        
+            var encontrado = false
+        
+            for i in 0..<livros.count {
+        
+                if livros[i].titulo == nomeLivro {
+        
+                    encontrado = true
+        
+                    if livros[i].disponibilidade {
+        
+                        print("O livro '\(nomeLivro)' está disponível.")
+                        print("Gostaria de alugar o livro? (s/n)")
+        
+                        let resposta = readLine() ?? ""
+        
+                        if resposta == "s" {
+                            livros[i].disponibilidade = false
+                            print("Alterando disponibilidade do livro '\(nomeLivro)'...")
+                            print("O livro '\(nomeLivro)' foi emprestado!")
+                        }
+        
+                    } else {
+                        print("O livro '\(nomeLivro)' está emprestado.")
+                    }
+                }
             }
         
+            if !encontrado {
+                print("Livro não encontrado.")
+            }
+                        
         default:
             print("Opção inválida")
     }
